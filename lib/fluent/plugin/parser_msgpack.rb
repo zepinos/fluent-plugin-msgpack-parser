695058efabc58af8f17937ff14a9dee0b587185e
zepinos <zepinos@gmail.com>
2020-07-03 18:39:13 +0900

    fix error

--- lib/fluent/plugin/parser_msgpack.rb
+++ lib/fluent/plugin/parser_msgpack.rb
@@ -1,10 +1,13 @@
+require 'fluent/parser'
+
 module Fluent
   class TextParser
     class MsgPackParser < Parser
       Plugin.register_parser("msgpack", self)
 
       def parse(text)
-        yield MessagePack.unpack(text)
+        yield Fluent::EventTime.now, MessagePack.unpack(text)
+        # yield nil, MessagePack.unpack(text)
       end
     end
   end
